a31cb73d249099aab81ee4201fd3d9f1bbdd2ece
Chebichii-Lab <chebichiiserem@gmail.com>
2024-03-07 20:56:33 +0300

    add comments

--- control_flow.dart
+++ control_flow.dart
@@ -1,5 +1,5 @@
 void main() {
-  var number = 10;
+  var number = 20;
   if (number > 10) {
     print('Your number is greater than 10');
   } else if (number == 10) {
@@ -8,3 +8,7 @@ void main() {
     print('Your number is less than 10');
   }
 }
+
+// if number is 10 result is YOUR NUMBER IS EQUAL TO 10
+// if number is 2 which is less than 10 result is YOUR NUMBER IS LESS THAN 10
+// if number is 20 which is greater than 10 reuslt is YOUR NUMBER IS GREATER THAN 10
